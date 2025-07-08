ff08af40a7abad531f4935a3fbea76ffe71e7398
IVP6 <ivan@padron.me>
2025-07-07 20:31:49 -0500

    Exercise 5 complete
    I misunderstood the instructions and thought it was asking for two numbers one: a favorite number & two: a number to square. The code still meets the requirements just has an extra number.

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,43 @@ using System;
 
 class Program
 {
+   
+   static void DisplayWelcom()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+   static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    { 
+        Console.Write("Please enter your favorite integer number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int number)
+    { 
+       // Console.Write("Give me a number to square: ");
+       // int number = int.Parse(Console.ReadLine());
+        return number * number;
+    }
+   
+    static void DisplayResult(string name, int favoriteNumber, int number, int squaredNumber)
+    {
+        Console.WriteLine($"Hello {name}, your favorite number is {favoriteNumber} and your number: {number} squared is {squaredNumber}.");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcom();
+        string userName = PromptUserName();
+        int favoriteNumber = PromptUserNumber();
+        Console.Write("Give me a number to square: ");
+        int number = int.Parse(Console.ReadLine());
+        int squaredNumber = SquareNumber(number);
+        DisplayResult(userName, favoriteNumber, number, squaredNumber);
     }
 }
