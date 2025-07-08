ab156a5561cb1a8e41fe14871b8bfd7e8e291781
IVP6 <ivan@padron.me>
2025-07-07 22:13:38 -0500

    Abstraction Complete
    Completed W02 Resume Assignment and achieved same output as the example.

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Google";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2015;
+        job1._endYear = 2020;
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Entry Level Software Engineer";
+        job2._startYear = 2025;
+        job2._endYear = 2027;
+
+        Resume resume1 = new Resume();
+        resume1._name = "John Doe";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1.Display();
+
+
     }
 }
