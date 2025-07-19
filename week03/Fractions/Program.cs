f8138768780b334955251c315438632db05e1f80
IVP6 <ivan@padron.me>
2025-07-19 15:36:59 -0500

    Week 3 Fractions Complete
    Still working on scripture program

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        //output a fraction and a decimal
+        Fraction first = new Fraction();
+        Console.WriteLine(first.GetFractionString());
+        Console.WriteLine(first.GetDecimalValue());
+        //output a new instance of fraction and a decimal
+
+        Fraction second = new Fraction(5);
+        Console.WriteLine(second.GetFractionString());
+        Console.WriteLine(second.GetDecimalValue());
+
+        Fraction third = new Fraction(3, 4);
+        Console.WriteLine(third.GetFractionString());
+        Console.WriteLine(third.GetDecimalValue());
+
+        Fraction fourth = new Fraction(1, 3);
+        Console.WriteLine(fourth.GetFractionString());
+        Console.WriteLine(fourth.GetDecimalValue());
+
     }
+    
 }
